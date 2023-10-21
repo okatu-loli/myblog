bc4b2964bac6b5e943a31dc0795dbebe8c1f2a21
千石 <CN_QianShi@hotmail.com>
2023-10-21 12:24:08 +0000

    Update name and contact information

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "范誉萧",
+  title: "学生",
+  email: "okatu@cnqs.moe",
+  gitHub: "okatu-loli",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
